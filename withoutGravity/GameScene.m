6f4ada098f9d199a704e323b3e18d252ef259353
LexLoki <pietroribeiropepe@hotmail.com>
2015-02-11 04:08:51 -0200

    Simple prototype version

--- withoutGravity/GameScene.m
+++ withoutGravity/GameScene.m
@@ -6,10 +6,22 @@
 //  Copyright (c) 2015 Pietro Ribeiro Pepe. All rights reserved.
 //
 
+/*   .txt instructions:
+ 
+ First comes the dimensions of the grid.
+ Second comes the number of entries for wall placement.
+    Then for each entry: 3 values, 2 are coordinates of the place, the other how many walls in that "square".
+        Followed by the type of each wall in that square(0-down, 1-right, 2-up, 3-left).
+ Third comes the number of entries for box placement.
+    Then for each entry: 1 value, the type of the box (0-objective, 1-support).
+        Followed by the coordinates of the place the box will be.
+*/
+
 #define BOX 1
-#define WALL 20 
+#define WALL 20
 
 #import "GameScene.h"
+@import CoreMotion;
 
 @interface GameScene()
 @property (nonatomic) UIButton *leftGravity;
@@ -17,10 +29,12 @@
 @end
 
 @implementation GameScene
-bool enableGravityChange;
+bool enableGravityChange, timerStarted;
 NSMutableArray *boxesArray;
 NSString *levelToLoad=@"stage001";
 CGRect gameRectangle;
+NSInteger boxesTotal, boxesCleared;
+CFTimeInterval timeCount;
 
 -(void)didMoveToView:(SKView *)view {
     enableGravityChange=false;
@@ -33,7 +47,7 @@ CGRect gameRectangle;
     
     //[self addChild:myLabel];
     [self setButtons];
-     
+    
 }
 
 -(void)setButtons{
@@ -47,6 +61,9 @@ CGRect gameRectangle;
     [self.rightGravity setBackgroundImage:[UIImage imageNamed:@"rightArrow"] forState:UIControlStateNormal];
     [self.rightGravity addTarget:self action:@selector(gravityAntiClockWise) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.rightGravity];
+    [self makeScenario];
+    timerStarted=false;
+    enableGravityChange=true;
 }
 
 -(void)gravityClockWise{
@@ -58,7 +75,10 @@ CGRect gameRectangle;
 }
 
 -(void)changeGravity:(NSInteger)direction{
-    self.physicsWorld.gravity=CGVectorMake(self.physicsWorld.gravity.dy*direction, -self.physicsWorld.gravity.dx*direction);
+    if(enableGravityChange){
+        enableGravityChange=false;
+        self.physicsWorld.gravity=CGVectorMake(self.physicsWorld.gravity.dy*direction, -self.physicsWorld.gravity.dx*direction);
+    }
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -79,11 +99,11 @@ CGRect gameRectangle;
     }
 }
 
--(void)makeScenario:(char*)filename{
+-(void)makeScenario{
     FILE *arq;
     NSInteger quantX, quantY;
     float taxax, taxay, x, y;
-    boxesArray = [[NSMutableArray alloc] initWithCapacity:10];
+    boxesTotal=boxesCleared=0;
     NSString* path = [[NSBundle mainBundle] pathForResource:levelToLoad ofType:@"txt"];
     //arq=fopen("/Users/Piupas/Desktop/JogoTeste/JogoTeste/fase1.txt","rt");
     arq=fopen([path UTF8String],"rt");
@@ -92,7 +112,6 @@ CGRect gameRectangle;
         return;
     }
     
-    NSInteger i,j,leitor;
     fscanf(arq,"%d%d",&quantX, &quantY);
     taxax=0.8*self.frame.size.width/quantX;
     taxay=0.8*self.frame.size.height/quantY;
@@ -107,21 +126,20 @@ CGRect gameRectangle;
     gameRectangle = CGRectMake(self.frame.size.width/2-x/2, self.frame.size.height/2+y/2, x, y);
     
     //Here we load the elements of the stage - walls only until now
-    for(i=0;i<quantY;i++)
-        for(j=0;j<quantX;j++){
-            if(fscanf(arq,"%d",&leitor)==0)
-                break;
-            if(leitor!=0)
-                [self place:leitor pos1:i pos2:j scalex:taxax scaley:taxay];
-        }
+    [self readWalls:arq taxax:taxax taxay:taxay];
+    
+    //Here we load the boxes
+    boxesArray = [[NSMutableArray alloc] initWithCapacity:2];
+    [self readBoxes:arq taxax:taxax taxay:taxay];
+    
     fclose(arq);
 }
 
 -(void)readWalls:(FILE*)arq
-           quant:(NSInteger)quant
            taxax:(float)taxax
            taxay:(float)taxay{
-    NSInteger lin, col, total, j, angle;
+    NSInteger lin, col, total, j, angle, quant;
+    fscanf(arq, "%d", &quant);
     for(NSInteger i=0;i<quant;i++){
         fscanf(arq, "%d%d%d", &lin, &col, &total);
         for(j=0;j<total;j++){
@@ -136,59 +154,97 @@ CGRect gameRectangle;
              col:(NSInteger)col
            taxax:(float)taxax
            taxay:(float)taxay{
-    CGFloat posx = gameRectangle.origin.x, posy;
+    
+    CGPoint pos = CGPointMake(gameRectangle.origin.x+col*taxax+taxax/2, gameRectangle.origin.y-lin*taxay-taxay/2);
+    CGFloat angle;
     switch(type){
         case 0:{
+            pos.y-=taxay/2;
+            angle=0;
             break;
         }
         case 1:{
+            pos.x+=taxax/2;
+            angle=0.5;
             break;
         }
         case 2:{
+            pos.y+=taxay/2;
+            angle=0;
             break;
         }
         case 3:{
+            pos.x-=taxax/2;
+            angle=0.5;
             break;
         }
         default: return;
     }
+    NSLog(@"WALL PUT");
+    SKSpriteNode *obj=[SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"mirror"]];
+    obj.zRotation=angle*M_PI;
+    obj.zPosition=5;
+    obj.xScale = taxax/obj.size.width;
+    obj.yScale = obj.xScale;
+    obj.position=pos;
+    obj.physicsBody=[SKPhysicsBody bodyWithRectangleOfSize:obj.size];
+    obj.physicsBody.dynamic=false;
+    [self addChild:obj];
+    
 }
 
--(void)place:(NSInteger)num pos1:(NSInteger)i pos2:(NSInteger)j scalex:(float)taxax scaley:(float)taxay{
-    NSString *name;
-    float angle = 0;
-    CGFloat physicsScale=1.0;
-    SKSpriteNode *obj;
-    CGFloat zPos = 3;
-    uint32_t bitmask;
-    switch (num){
-            /* ** ADICIONAR MAIS ** */
-        default:
-            return;
+-(void)readBoxes:(FILE*)arq
+           taxax:(float)taxax
+           taxay:(float)taxay{
+
+    NSInteger quant, lin, col, type;
+    fscanf(arq, "%d", &quant);
+    for(NSInteger i=0;i<quant;i++){
+        fscanf(arq,"%d%d%d", &type, &lin, &col);
+        [self placeBox:type lin:lin col:col taxax:taxax taxay:taxay];
     }
-    obj = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:name]];
     
-    [self adjustNodeToSize:CGSizeMake(taxax, taxay) node:obj angle:angle bitmask:bitmask];
-    obj.position = CGPointMake(gameRectangle.origin.x+j*taxax+taxax/2, gameRectangle.origin.y-i*taxay-taxay/2); //(40,540) is the top border location
-    if(bitmask==mirrorCategory)
-        obj.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(obj.size.width/physicsScale, obj.size.height/10)];
-    else
-        obj.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(obj.size.width/physicsScale, obj.size.height/physicsScale)];
-    obj.zRotation = angle*M_PI/4;
-    obj.physicsBody.dynamic=false;
-    
-    obj.physicsBody.categoryBitMask=bitmask;
-    //obj.physicsBody.collisionBitMask=0;
-    obj.physicsBody.contactTestBitMask=ballCategory;
-    obj.physicsBody.friction = 0.0f;
+}
+
+-(void)placeBox:(NSInteger)type
+            lin:(NSInteger)lin
+            col:(NSInteger)col
+          taxax:(float)taxax
+          taxay:(float)taxay{
+    switch(type){
+        case 0:{
+            boxesTotal++;
+            break;
+        }
+        case 1:{
+            
+            break;
+        }
+    }
+    SKSpriteNode *obj=[SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"boxcolor"]];
+    obj.zPosition=5;
+    obj.xScale = 0.75*taxax/obj.size.width;
+    obj.yScale = obj.xScale;
+    obj.position=CGPointMake(gameRectangle.origin.x+col*taxax+taxax/2, gameRectangle.origin.y-lin*taxay-taxay/2);
+    obj.physicsBody=[SKPhysicsBody bodyWithRectangleOfSize:obj.size];
     
     [self addChild:obj];
-    obj.zPosition=zPos;
 }
-
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+    if(!enableGravityChange){
+        if(!timerStarted){
+            timerStarted=true;
+            timeCount=currentTime;
+        }
+        else{
+            if(currentTime-timeCount>=1.0){
+                enableGravityChange=true;
+                timerStarted=false;
+            }
+        }
+    }
 }
 
 @end
